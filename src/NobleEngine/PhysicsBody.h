1177f58b2a202b65e37df7f205effabe42bd2582
Samuel Brown <samuelbrown3199@gmail.com>
2021-01-17 06:42:13 +0000

    Update PhysicsBody.h

--- src/NobleEngine/PhysicsBody.h
+++ src/NobleEngine/PhysicsBody.h
@@ -65,44 +65,45 @@ namespace NobleEngine
 			if (!transform)
 			{
 				std::cout << "Could not find transform on entity " << entityID << ". Make sure transform is attached before physics body component!" << std::endl;
-				GetEntity()->RemoveComponent<PhysicsBody>();
-				return;
 			}
-			transform->checkForPhysicsBody = false;
-			colliderShape = _shape;
-
-			switch (colliderShape)
+			else
 			{
-			case box:
-				shape = new btBoxShape(btVector3(transform->scale.x, transform->scale.y, transform->scale.z));
-				break;
-			case sphere:
-				shape = new btSphereShape(transform->scale.x);
-				break;
+				transform->checkForPhysicsBody = false;
+				colliderShape = _shape;
+
+				switch (colliderShape)
+				{
+				case box:
+					shape = new btBoxShape(btVector3(transform->scale.x, transform->scale.y, transform->scale.z));
+					break;
+				case sphere:
+					shape = new btSphereShape(transform->scale.x);
+					break;
+				}
+				Application::GetPhysicsWorld()->AddCollider(shape);
+
+				bodyTransform.setOrigin(btVector3(transform->position.x, transform->position.y, transform->position.z));
+				bodyTransform.setRotation(btQuaternion(transform->rotation.x, transform->rotation.y, transform->rotation.z));
+
+				mass = _mass;
+				btVector3 localInertia(0, 0, 0);
+				bool isDynamic = (mass != 0.f);
+				if (isDynamic)
+				{
+					shape->calculateLocalInertia(mass, localInertia);
+				}
+				btDefaultMotionState* motionState = new btDefaultMotionState(bodyTransform);
+				btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, motionState, shape, localInertia);
+				rbInfo.m_linearDamping = .2f;
+				rbInfo.m_angularDamping = .2f;
+				rbInfo.m_restitution = 0.35;
+
+				rigidBody = new btRigidBody(rbInfo);
+				rigidBody->setFriction(.5f);
+				rigidBody->setRollingFriction(.5f);
+
+				Application::GetPhysicsWorld()->AddRigidbody(rigidBody);
 			}
-			Application::GetPhysicsWorld()->AddCollider(shape);
-
-			bodyTransform.setOrigin(btVector3(transform->position.x, transform->position.y, transform->position.z));
-			bodyTransform.setRotation(btQuaternion(transform->rotation.x, transform->rotation.y, transform->rotation.z));
-
-			mass = _mass;
-			btVector3 localInertia(0, 0, 0);
-			bool isDynamic = (mass != 0.f);
-			if (isDynamic)
-			{
-				shape->calculateLocalInertia(mass, localInertia);
-			}
-			btDefaultMotionState* motionState = new btDefaultMotionState(bodyTransform);
-			btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, motionState, shape, localInertia);
-			rbInfo.m_linearDamping = .2f;
-			rbInfo.m_angularDamping = .2f;
-			rbInfo.m_restitution = 0.35;
-
-			rigidBody = new btRigidBody(rbInfo);
-			rigidBody->setFriction(.5f);
-			rigidBody->setRollingFriction(.5f);
-
-			Application::GetPhysicsWorld()->AddRigidbody(rigidBody);
 		}
 	};
 }
