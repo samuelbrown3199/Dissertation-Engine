5e2c0640e25209470c82e6a83d79c9f33dd65a18
Samuel Brown <samuelbrown3199@gmail.com>
2021-02-10 00:08:06 +0000

    Early Light system functioning

--- src/TestProgram/RotatorSystem.cpp
+++ src/TestProgram/RotatorSystem.cpp
@@ -5,4 +5,5 @@ SetupComponentList(Rotator);
 void RotatorSystem::OnUpdate(std::shared_ptr<Rotator> comp)
 {
 	comp->transform->rotation.z += comp->rotationRate;
+	std::cout << comp->transform->rotation.z << std::endl;
 }
